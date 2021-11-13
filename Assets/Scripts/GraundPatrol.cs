5b85e19f311a5f12ffc4513b9e5bdd67d8f247fc
Lilya20 <l31326976@gmail.com>
2021-11-13 21:38:49 +0300

    перелет от одной точки в другую

--- Assets/Scripts/GraundPatrol.cs
+++ Assets/Scripts/GraundPatrol.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.UIElements;
 using UnityEngine;
-
+// червяк 
 public class GraundPatrol : MonoBehaviour
 {
     public float speed = 1f;
@@ -19,8 +19,22 @@ public class GraundPatrol : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector2.left*speed*Time.deltaTime);//червячок двигается влево с опр скоростью и плавно(Time.deltaTime)
-        RaycastHit2D groundInfo= Physics2D.Raycast() // 1- объект столкновения луча в пространстве 
-        
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetect.position, Vector2.down, 1f); // 1- объект столкновения луча в  2D пространстве / (1- откуда будет исходить луч.2-куда он будет направлен, 3- какой длины будет луч)
+
+        if (groundInfo.collider == false)
+        {
+            if (moveLeft == true)
+            {
+                transform.eulerAngles = new Vector3(0,180,0); // eulerAngles - угол в градусах
+                moveLeft = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0,0,0);
+                moveLeft = true;
+            }
+        }
+
     }
 }
 
